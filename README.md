193f31c321b5331cd02168738e924a76fdc34efc
Tuan Luong <tdluong@crimson.ua.edu>
2024-10-04 10:59:30 -0500

    add updated ros2 commands for logging every relevant data and update matlab script to process them

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This repo contains codes for multi modal extrensic calibration for NILE dataset.
 For synthetic data generation. Follow instruction from ROS2 [wiki](https://docs.ros.org/en/humble/Tutorials/Advanced/Simulators/Gazebo/Gazebo.html)
 * ROS2 Humble
 * Ignition Gazebo Fortress
+* MATLAB with ROS packages for processing
 
 ## Run sim
 ```
@@ -15,9 +16,22 @@ ign gazebo -r <scene.world>
 ```
 
 ## Start ros gz bridge
+We want to only transfer the thermal and optical images as well as point cloud to be saved as rosbag for processing with Matlab
 To transport data from gazebo to ros2 sim, run the following in a sourced terminal
 ```
-ros2 run ros_ign_bridge parameter_bridge /lidar/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked
+ros2 run ros_ign_bridge parameter_bridge /lidar/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked \
+     /thermal_camera_8bit_left/image@sensor_msgs/msg/Image@gz.msgs.Image \
+     /thermal_camera_8bit_right/image@sensor_msgs/msg/Image@gz.msgs.Image \
+     /camera_left/image@sensor_msgs/msg/Image@gz.msgs.Image \
+     /camera_right/image@sensor_msgs/msg/Image@gz.msgs.Image 
 ```
+
+## Recording as bag
+In a sourced terminal, run the following
+
+```
+ros2 bag record /lidar/points /thermal_camera_8bit_left/image /thermal_camera_8bit_right/image /camera_left/image /camera_right/image
+```
+
 
   
--- test_gazebo_data.m
+++ test_gazebo_data.m
@@ -2,14 +2,37 @@ close all
 clear
 clc
 
-bagReader = ros2bagreader('/home/tuan/rosbag2_2024_10_04-10_10_32/');
+% Object to read bag file
+bag_reader = ros2bagreader('/home/tuan/rosbag2_2024_10_04-10_50_09/');
 
-pcBag = select(bagReader,'Topic','/lidar/points');
+% Get data based on topics
+left_thermal_bag = select(bag_reader,'Topic','/thermal_camera_8bit_left/image');
+right_thermal_bag = select(bag_reader,'Topic','/thermal_camera_8bit_right/image');
+left_optical_bag = select(bag_reader,'Topic','/camera_left/image');
+right_optical_bag = select(bag_reader,'Topic','/camera_right/image');
+lidar_bag= select(bag_reader,'Topic','/lidar/points');
 
-pcMsg = readMessages(pcBag);
+% Get message
+left_thermal_msg = readMessages(left_thermal_bag);
+right_thermal_msg = readMessages(right_thermal_bag);
+left_optical_msg = readMessages(left_optical_bag);
+right_optical_msg = readMessages(right_optical_bag);
+lidar_msg = readMessages(lidar_bag);
 
-pc = pointCloud(rosReadXYZ(pcMsg{1,1}));
+left_thermal_img = rosReadImage(left_thermal_msg{1,1});
+right_thermal_img = rosReadImage(right_thermal_msg{1,1});
+left_optical_img = rosReadImage(left_optical_msg{1,1});
+right_optical_img = rosReadImage(right_optical_msg{1,1});
+pc = pointCloud(rosReadXYZ(lidar_msg{1,1}));
 
+figure()
+imshow(horzcat(left_thermal_img, right_thermal_img));
+
+
+figure()
+imshow(horzcat(left_optical_img, right_optical_img))
+
+figure()
 pcshow(pc,"MarkerSize",10)
 xlim([0, 6]);
 ylim([-4 4]);
